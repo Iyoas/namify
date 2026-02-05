b41850b72ff5cb47f92d78876fe0f703ceba1c77
Iyoas <iyoasennay@gmail.com>
2026-02-05 22:23:25 +0100

    correct deeplink

--- src/lib/affiliate/godaddy.ts
+++ src/lib/affiliate/godaddy.ts
@@ -1,7 +1,10 @@
-const GODADDY_AFFILIATE_BASE_URL =
-  "https://click.godaddy.com/affiliate?isc=cjcfos3&url=https://www.godaddy.com/domainsearch/find?domainToCheck=";
+const GODADDY_AFFILIATE_BASE_URL = "https://click.godaddy.com/affiliate?isc=cjcfos3&url=";
+const GODADDY_SEARCH_BASE_URL =
+  "https://www.godaddy.com/domainsearch/find?domainToCheck=";
 const GODADDY_ELB_DAYS_PARAM = "&cjelbDays=45";
 
 export function buildGoDaddySearchUrl(domain?: string): string {
-  return `${GODADDY_AFFILIATE_BASE_URL}${domain ?? ""}${GODADDY_ELB_DAYS_PARAM}`;
+  const godaddyUrl = `${GODADDY_SEARCH_BASE_URL}${domain ?? ""}`;
+  const encoded = encodeURIComponent(godaddyUrl);
+  return `${GODADDY_AFFILIATE_BASE_URL}${encoded}${GODADDY_ELB_DAYS_PARAM}`;
 }

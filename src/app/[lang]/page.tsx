e901c927284b5f683ec6cf957e8be27793c3dca0
Iyoas <iyoasennay@gmail.com>
2026-02-23 19:05:34 +0100

    s

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -57,7 +57,7 @@ export default async function DomainGeneratorLanding({ params }: Props) {
       <Hero lang={lang} messages={messages} />
       <HowItWorks messages={messages} />
       <HowWeUseTool messages={messages} />
-      {/* <IndustryGenerators messages={messages} /> */}
+      <IndustryGenerators messages={messages} />
       <Faq messages={messages} />
       <Contact messages={messages} />
     </main>
